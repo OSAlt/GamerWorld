413c31cbe5a5ffcc2424889504657afe84ac6955
corey drew bruce <cdfrosty@gmail.com>
2020-03-05 23:55:13 +1100

    Updated main code, will be used or next version and will have a tray icon

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const {app, BrowserWindow, shell} = require('electron')
+const {app, BrowserWindow, Tray, Menu} = require('electron')
 const path = require('path')
 
-// Disabled VSync due to WebGL issues
 app.commandLine.appendSwitch('disable-frame-rate-limit')
 //Dual GPU Support
 app.commandLine.appendSwitch('supports-dual-gpu=true')
@@ -10,7 +9,6 @@ app.commandLine.appendSwitch('disable-gpu-sandbox')
 app.commandLine.appendSwitch('ignore-gpu-blacklist', 'true');
 app.commandLine.appendSwitch('disable-composited-antialiasing')
 //app.commandLine.appendSwitch('disable-gpu-memory-buffer-compositor-resources')
-app.commandLine.appendSwitch('enable-experimental-fullscreen-exit-ui')
 app.commandLine.appendSwitch('enable-gpu-async-worker-context')
 app.commandLine.appendSwitch('enable-hardware-overlays')
 app.commandLine.appendSwitch('enable-native-gpu-memory-buffers')
@@ -21,35 +19,37 @@ app.commandLine.appendSwitch('force-gpu-rasterization')
 //Fixes WebGL support on old GPUS that get false unsupported message.
 app.commandLine.appendSwitch('ignore-gpu-blacklist', 'true');
 
-// Optional: Specify flash version, for example, v17.0.0.169
-app.commandLine.appendSwitch('ppapi-flash-version', '30.0.0.154')
+let mainWindow
 
-let main = null
-const createWindow = () => {
-  main = new BrowserWindow({
-   width: 1350,
-   height: 900,
-   icon: __dirname + '/icon.png',
-   webPreferences: {
-     plugins: false,
-     nodeIntegration: false,
-     enableRemoteModule: false},
-     show: false
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1350,
+    height: 900,
+    icon: __dirname + 'icon.png',
+    webPreferences: {
+      plugins: false,
+      nodeIntegration: false,
+      enableRemoteModule: false},
+      show: false
   })
 
   let splash = new BrowserWindow({width: 500, height: 500, transparent: true, frame: false});
-  splash.loadURL('file://' + __dirname + '/index.html') // Load splash screen
-  main.loadURL('https://gw-ui.netlify.com',{ userAgent: "Mozilla/5.0 rv:73.0) Gecko/20100101 Firefox/73.0'"})
+  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://gw-ui.netlify.com',{ userAgent: "Mozilla/5.0 rv:73.0) Gecko/20100101 Firefox/73.0'"})
 
-  main.once('ready-to-show', () => {
+  mainWindow.once('ready-to-show', () => {
     splash.destroy();
-    main.maximize() // start maximized
-    main.setMenuBarVisibility(false)
-    main.setMenu(null)
-    main.$ = main.jQuery = require('jquery');
-    main.show();
+    mainWindow.maximize() // start maximized
+    mainWindow.setMenuBarVisibility(false)
+    mainWindow.setMenu(null)
+    mainWindow.$ = mainWindow.jQuery = require('jquery');
+    mainWindow.show();
   });
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 
+//WILL BE USED ONCE PROPERLY IMPLEMENTED
   //main.webContents.on('new-window', function(e, url) {
     // make sure local urls stay in electron perimeter
     //if('file://' === url.substr(0, 'file://'.length)) {
@@ -61,24 +61,41 @@ const createWindow = () => {
     //shell.openExternal(url);
   //});
 
-  main.webContents.on('will-navigate', ev => {
-    ev.preventDefault()
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
   })
+
+  const tray = new Tray(
+    path.resolve(__dirname, 'icon.png'),
+  );
+  tray.setContextMenu(
+    Menu.buildFromTemplate([
+      {
+        label: "Quit",
+        click() {
+          tray.destroy();
+          app.quit();
+        },
+      },
+    ]),
+  );
 }
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // Fix OS X issue where applications and their menu bar
-  // stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin')
-    app.quit()
-})
-
-app.on('activate', () => {
-  // Fix OS X issue where windows in the app are re-created when the
-  // dock icon is clicked and there are no other windows open.
-  if (main === null)
-    createWindow()
-})
-
 app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
